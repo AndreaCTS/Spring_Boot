4caf59757e6f81615ee9c6139979a14233f8bdec
unknown <juan.bernal05@usa.edu.co>
2023-09-15 12:55:52 -0500

    Corrijo nombrado de paquetes

--- src/main/java/com/example/demo/ControllerDepartamento_1.java
+++ src/main/java/com/example/demo/ControllerDepartamento_1.java
@@ -1,11 +1,9 @@
-package controller;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.example.demo;
 
-import com.example.demo.Departamento;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
