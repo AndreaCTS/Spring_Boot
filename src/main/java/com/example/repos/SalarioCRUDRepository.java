2e30a795f9ce1f2d786f508af0af9d8c291be071
Sala White <white@academia.loc>
2023-09-15 11:23:46 -0500

    Creacion Repositorio Salario

---
+++ src/main/java/com/example/repos/SalarioCRUDRepository.java
@@ -0,0 +1,9 @@
+package com.example.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.model.Salario;
+
+public interface SalarioCRUDRepository extends CrudRepository<Salario, Integer>{
+
+}
---
+++ src/main/java/com/example/repos/SalarioRepository.java
@@ -0,0 +1,27 @@
+package com.example.repos;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import com.example.model.Salario;
+
+@Repository
+public class SalarioRepository {
+	@Autowired
+    private	SalarioCRUDRepository SalarioCRUDRepository;
+
+
+    public List<Salario> getAll(){
+        return (List<Salario>) SalarioCRUDRepository.findAll();
+    }
+    
+    public Salario save(Salario Salario){
+        return SalarioCRUDRepository.save(Salario);
+    }
+    public void delete(Salario Salario){
+        SalarioCRUDRepository.delete(Salario);
+    }
+}
