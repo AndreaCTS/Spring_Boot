8f7c21f901cd7a619385b6424b9462441754346e
unknown <juan.bernal05@usa.edu.co>
2023-09-15 12:08:18 -0500

    Creo clase controlador para departamento

---
+++ src/main/java/com/example/demo/ControllerDepartamento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author USUARIO
+ */
+@RestController
+public class ControllerDepartamento {
+    
+}
--- src/main/java/com/example/demo/Departamento.java
+++ src/main/java/com/example/demo/Departamento.java
@@ -2,16 +2,52 @@ package com.example.demo;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.persistence.Table;
+import java.util.List;
 
 @Entity
+@Table(name="Departamento")
 public class Departamento{
     @Id
     private Integer idDepartamento;
     
     private String nombreDept;
     private String descDept;
-    private List<Empleado>
+    
+    private List<Empleado> empleados;
+
+    public Integer getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(Integer idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public String getNombreDept() {
+        return nombreDept;
+    }
+
+    public void setNombreDept(String nombreDept) {
+        this.nombreDept = nombreDept;
+    }
+
+    public String getDescDept() {
+        return descDept;
+    }
+
+    public void setDescDept(String descDept) {
+        this.descDept = descDept;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+    
+    
     
 }
