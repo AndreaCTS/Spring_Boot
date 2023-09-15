0bbb8fe6df26422c6257ebadc0b32fd6b850cfd2
Carlos <carlos.bermudez01@usa.edu.co>
2023-09-15 17:32:55 -0500

    create crudRepos

---
+++ src/main/java/repository/ReunionRepository.java
@@ -0,0 +1,34 @@
+package repository;
+import model.*;
+import repository.crudrepository.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ReunionRepository {
+    @Autowired
+    private ReunionCrudRepository reunionCrudRepository;
+
+    public List<Reunion> getAll() {
+        return (List<Reunion>) reunionCrudRepository.findAll();
+    }
+
+    public Optional<Reunion> getReunion(Long id) {
+        return reunionCrudRepository.findById(id);
+    }
+
+    public Reunion save(Reunion reunion) {
+        return reunionCrudRepository.save(reunion);
+    }
+
+    public void delete(Reunion reunion) {
+        reunionCrudRepository.delete(reunion);
+    }
+}
---
+++ src/main/java/repository/crudrepository/ReunionCrudRepository.java
@@ -0,0 +1,11 @@
+package repository.crudrepository;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import model.*;
+
+public interface ReunionCrudRepository extends CrudRepository<Reunion, Long> {
+
+    
+	
+}
