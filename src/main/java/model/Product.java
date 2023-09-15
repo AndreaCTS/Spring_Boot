7500d0961af0c95c5e9b0401fd82ff5916278d96
Oscar Miranda Puentes <oscar.miranda@usa.edu.co>
2023-09-15 12:07:10 -0500

    Creación de interfaces de Product

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -12,5 +12,29 @@ public class Product {
 
     private int id;
     private String name;
-    private String modelo;
+    private String marca;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 }
